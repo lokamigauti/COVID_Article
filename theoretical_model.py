828ccdafb6308918198a1b40b208158b866d8038
gab <gabrielmpp2@gmail.com>
2020-04-04 16:02:17 +0100

    update

---
+++ theoretical_model.py
@@ -0,0 +1,67 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import xarray as xr
+import pandas as pd
+locations_x, locations_y = np.meshgrid(np.arange(0,10,1), np.arange(0, 10, 1))
+from scipy.signal import gaussian
+plt.style.use('seaborn')
+
+def run_simulation(particulate, dt=1/24, ndays=90, popsize=1e6, removal_rate=1e-3, infection_rate=2e-2):
+    nt = int(ndays / dt)
+    I = np.zeros(nt)
+    R = np.zeros(nt)
+    S = np.zeros(nt)
+    I[0] = 1  # first individual
+    R[0] = 0  # removed
+    S[0] = popsize
+    alpha = removal_rate
+    beta = infection_rate + infection_rate * particulate
+    for t in range(nt - 1):
+        R[t + 1] = R[t] + dt * alpha*I[t]
+        I[t + 1] = I[t] + dt * (beta[t]*S[t]*I[t] -alpha*I[t])
+        S[t + 1] = S[t] - dt * beta[t]*S[t]*I[t]
+    return R, I, S, beta
+
+
+ndays = 90
+dt = 1/24
+nt = int(ndays / dt)
+times = np.arange(0, ndays, dt)  # time in days
+particulate = 10 * gaussian(nt, std = 100)
+particulate_control = np.zeros(nt)
+plt.plot(times, particulate)
+plt.ylabel('Concentration of particulate')
+plt.xlabel('Time since outbreak')
+
+plt.show()
+
+control_R, control_I, control_S, beta_control = run_simulation(particulate_control,
+                                                 dt=dt, ndays=ndays, infection_rate=0.5e-6,
+                                                 removal_rate=0.5e-1)
+
+R, I, S, beta = run_simulation(particulate, dt=dt, ndays=ndays, infection_rate=0.5e-6, removal_rate=0.5e-1  )
+
+I = xr.DataArray(I, dims=['time'], coords=dict(time=times))
+R = xr.DataArray(R, dims=['time'], coords=dict(time=times))
+S = xr.DataArray(S, dims=['time'], coords=dict(time=times))
+control_I = xr.DataArray(control_I, dims=['time'], coords=dict(time=times))
+control_R = xr.DataArray(control_R, dims=['time'], coords=dict(time=times))
+control_S = xr.DataArray(control_S, dims=['time'], coords=dict(time=times))
+
+plt.style.use('seaborn')
+plt.figure(figsize=[20, 20])
+da_control = xr.concat([ control_I, control_R, control_S],
+               pd.Index([ 'control_I', 'control_R', 'control_S'], name='Population type'))
+da_experiment = xr.concat([ I, R, S],
+               pd.Index([ 'I', 'R', 'S'], name='Population type'))
+da = xr.concat([da_experiment, da_control], pd.Index(['Experiment', 'Control'], name='Experiment type'))
+da.plot.line(x='time', row='Experiment type')
+plt.ylabel('Population #')
+plt.xlabel('Time since outbreak started (days)')
+plt.show()
+
+(da_experiment - da_control.values).plot.line(x='time')
+
+plt.plot(beta_control)
+
+beta
